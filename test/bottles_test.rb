e2768018a0b71cc5058210bf37babe4d9088828d
Alberto De Lucca <albertodelucca@gmail.com>
2016-09-15 23:34:48 -0400

    Fix test for Bottle.verses(99,98)

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -41,7 +41,7 @@ class BottlesTest < Minitest::Test
     expected = "99 bottles of beer on the wall, " +
       "99 bottles of beer.\n" +
       "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n" +
+      "98 bottles of beer on the wall.\n" + "\n" +
       "98 bottles of beer on the wall, " +
       "98 bottles of beer.\n" +
       "Take one down and pass it around, " +
