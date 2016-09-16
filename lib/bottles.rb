86fb1e604e8d34cc10e84cd438cbc1814021637e
Alberto De Lucca <albertodelucca@gmail.com>
2016-09-16 00:25:35 -0400

    Refactor to abstract away when verse is 2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,20 +7,22 @@ class Bottle
         "Go to the store and buy some more, " +
         "99 bottles of beer on the wall.\n"
     when 1
-      "#{number} bottle of beer on the wall, " +
-        "#{number} bottle of beer.\n" +
+      "#{number} #{container(number)} of beer on the wall, " +
+        "#{number} #{container(number)} of beer.\n" +
         "Take one down and pass it around, " +
-        "no more bottles of beer on the wall.\n"
-    when 2
-      "#{number} bottles of beer on the wall, " +
-        "#{number} bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "#{number - 1} bottle of beer on the wall.\n"
+        "no more #{container(number - 1)} of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, " +
-        "#{number} bottles of beer.\n" +
+      "#{number} #{container(number)} of beer on the wall, " +
+        "#{number} #{container(number)} of beer.\n" +
         "Take one down and pass it around, " +
-        "#{number - 1} bottles of beer on the wall.\n"
+        "#{number - 1} #{container(number - 1)} of beer on the wall.\n"
+    end
+  end
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
     end
   end
   def verses(up,down)
