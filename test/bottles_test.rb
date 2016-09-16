a9c8aa82c44092674e0f1442d1e441877961a017
Alberto De Lucca <albertodelucca@gmail.com>
2016-09-15 23:22:58 -0400

    Add passing code and test for Bottle.verse(0)

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -30,4 +30,11 @@ class BottlesTest < Minitest::Test
       "no more bottles of beer on the wall.\n"
     assert_equal expected, Bottle.new.verse(1)
   end
+  def test_verse_0
+    expected = "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
+    assert_equal expected, Bottle.new.verse(0)
+  end
 end
