d0b0c84f03254af36706be8022fe619da5e61bb1
Alberto De Lucca <albertodelucca@gmail.com>
2016-09-15 22:58:24 -0400

    Add passing code for Bottle.verse(99) test

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -2,4 +2,11 @@ require 'test_helper'
 require 'bottles'
 
 class BottlesTest < Minitest::Test
+  def test_a_verse
+    expected = "99 bottles of beer on the wall, " +
+      "99 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "98 bottles of beer on the wall.\n"
+    assert_equal expected, Bottle.new.verse(99)
+  end
 end
